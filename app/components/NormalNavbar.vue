6f9121146a8b7375495ae1322ef8143ef5356e77
Hideo Oshima <orimajp@gmail.com>
2020-03-20 22:57:33 +0900

    最低限のナビゲーションを作成、不要ファイル削除他

---
+++ app/components/NormalNavbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar app clipped-left dark dense class="primary">
+    <v-btn icon @click="goTop">
+      <v-icon>
+        mdi-home
+      </v-icon>
+    </v-btn>
+    <v-toolbar-title v-text="pageTitle" />
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    pageTitle: {
+      type: String,
+      default: () => {
+        return 'ドキュメント管理アプリモック'
+      }
+    }
+  },
+  methods: {
+    goTop(): void {
+      this.$router.push('/')
+    }
+  }
+})
+</script>
